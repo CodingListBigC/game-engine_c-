73ea4a52c840bc6b5c6232b522dccd4804a1328c
Christian List <dragondriver8@outlook.com>
2026-04-07 10:20:38 -0400

    25:(texture-loader): load texture from image path
    - Add library for image loading.
    - Add new class to load a texture class.

---
+++ src/utils/texture/textureLoader.h
@@ -0,0 +1,9 @@
+#ifndef TEXTURE_LOADER_H
+#define TEXTURE_LOADER_H
+
+#include <GL/glew.h> // Or your specific OpenGL loader
+#include <string>`
+
+inline GLuint loadTexture(const char *path);
+
+#endif // TEXTURE_LOADER_H
